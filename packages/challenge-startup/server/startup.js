a3acc0add915411b4bd4ef0eff801b8412f3e4f9
Dmitrijs Balcers <dmitrijs.balcers@d8corp.com>
2015-05-05 23:19:09 +0300

    Add test data

--- packages/challenge-startup/server/startup.js
+++ packages/challenge-startup/server/startup.js
@@ -4,13 +4,13 @@
 //    status: 'won'
 //});
 
-//Meteor.defer(function () {
-//
-//    for (var i = 0; i < 1000; i++) {
-//        Bookings.insert({
-//            price: Math.floor((Math.random() * 1000) + 100),
-//            date: Date.now(),
-//            status: 'won'
-//        });
-//    }
-//});
+Meteor.defer(function () {
+
+    for (var i = 0; i < 1000; i++) {
+        Bookings.insert({
+            price: Math.floor((Math.random() * 1000) + 100),
+            date: Date.now(),
+            status: 'won'
+        });
+    }
+});
